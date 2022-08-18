84a85d6bd519b8f65ca8d51f52b04d699d96e813
vinviiy <100759366+vinviiy@users.noreply.github.com>
2022-08-18 14:23:09 +0800

    Add files via upload

---
+++ jhinsdesign的副本/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button ,{ButtonType,ButtonSize}from './components/Button/button';
+
+function App() {
+ 
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Button autoFocus> Hello </Button>
+        <Button onClick={()=>{alert('sssss')}}> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
